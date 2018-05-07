2b7cecdf189280c2515787ffd967da2560b7b43d
lfryemason <lfryemason@unm.edu>
2018-05-07 11:40:52 -0600

    Added tasty testing framework

--- TirelessTracker/src/MatchData.hs
+++ TirelessTracker/src/MatchData.hs
@@ -20,11 +20,4 @@ data Result = Win | Draw | Loss deriving (Eq, Show, Ord)
 
 data Deck = Deck String deriving (Show)
 
-m = Match {matchID = 0, 
-           myDeck = Deck "D&T", 
-           oppDeck = Deck "Unknown", 
-           result = (Win, 2, 1, 0),
-           date = fromGregorian 2018 4 17,
-           eventType = "TuesdayModern"
-          }
 
--- TirelessTracker/test/Spec.hs
+++ TirelessTracker/test/Spec.hs
@@ -1,18 +1,23 @@
 import MatchData
 import Data.Time
+import Test.Tasty
+import Test.Tasty.HUnit
 
 main :: IO ()
-main = putStrLn (show testMatches)
+main = defaultMain tests
 
-testMatches :: Bool
-testMatches = (show m) == mStr
+tests :: TestTree
+tests = testGroup "Tests" [unitTests]
+
+unitTests = testGroup "Unit tests"
+        [testCase "Match show test" $ (show m) `compare` mStr @?= EQ ]
     where
         m = Match {matchID = 0, 
-           myDeck = Deck "D&T", 
+           myDeck = Deck "DeckName", 
            oppDeck = Deck "Unknown", 
-           result = (Win, 2, 1, 0),
+           result = (Win, 0, 0, 0),
            date = fromGregorian 2018 4 17,
-           eventType = "TuesdayModern"
+           eventType = "Event Name"
           }
-        mStr = "Match {matchID = 0, myDeck = Deck \"D&T\", oppDeck = Deck \"Unknown\", result = (Win,2,1,0), date = 2018-04-17, eventType = \"TuesdayModern\"}"
+        mStr = "Match {matchID = 0, myDeck = Deck \"DeckName\", oppDeck = Deck \"Unknown\", result = (Win,0,0,0), date = 2018-04-17, eventType = \"Event Name\"}"
 
